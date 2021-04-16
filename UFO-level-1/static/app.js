64121bb824b1fafc87674cf89b7fc4c523108a3d
Mike Hills <hills072@umn.edu>
2021-04-16 15:12:29 -0500

    created filteredData variable to handle matching user input for datetime.  Confirmed with console.log

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -1,30 +1,8 @@
 //console.log("app.js loaded")
 
-var button = d3.select("#filter-btn");
-
-var inputField = d3.select("#datetime");
-
-
-function handleClick() {
-
-    var inputValue = inputField.property("value");
-    console.log("A button was clicked!");
-    console.log(inputValue);
-  
-    // We can use d3 to see the object that dispatched the event
-    console.log(d3.event.target);
-  }
-  
-  // We can use the `on` function in d3 to attach an event to the handler function
-button.on("click", handleClick);
-
-
-
-
-
-
 var tableData = data;
-
+var button = d3.select("#filter-btn");
+var inputField = d3.select("#datetime");
 var tbody = d3.select("tbody");
 
 tableData.forEach(function(ufoData) {
@@ -36,3 +14,23 @@ tableData.forEach(function(ufoData) {
     });
  
 });
+
+function handleClick() {
+
+    d3.event.preventDefault();
+
+    var inputValue = inputField.property("value");
+
+    console.log("A button was clicked!");
+    console.log(inputValue);
+  
+    // We can use d3 to see the object that dispatched the event
+    console.log(d3.event.target);
+
+    var filteredData = tableData.filter(ufoData => ufoData.datetime === inputValue);
+    console.log(filteredData);
+
+  };
+  
+  // We can use the `on` function in d3 to attach an event to the handler function
+button.on("click", handleClick);
