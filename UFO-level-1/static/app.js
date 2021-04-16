cc474b51617ad88d91bd6956980c9f923f163ee6
Mike Hills <hills072@umn.edu>
2021-04-16 14:47:25 -0500

    added event handlers for button and input fields; confirmed functionality of each with console.log

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -1,5 +1,28 @@
 //console.log("app.js loaded")
 
+var button = d3.select("#filter-btn");
+
+var inputField = d3.select("#datetime");
+
+
+function handleClick() {
+
+    var inputValue = inputField.property("value");
+    console.log("A button was clicked!");
+    console.log(inputValue);
+  
+    // We can use d3 to see the object that dispatched the event
+    console.log(d3.event.target);
+  }
+  
+  // We can use the `on` function in d3 to attach an event to the handler function
+button.on("click", handleClick);
+
+
+
+
+
+
 var tableData = data;
 
 var tbody = d3.select("tbody");
