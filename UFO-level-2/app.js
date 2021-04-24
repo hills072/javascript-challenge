eb156dc5c7c7002000c6cb8d19fb7a7f61c64073
Mike Hills <hills072@umn.edu>
2021-04-24 13:21:37 -0500

    added comments to final code

--- UFO-level-2/app.js
+++ UFO-level-2/app.js
@@ -1,13 +1,11 @@
-//console.log("app.js loaded")
+console.log("app.js loaded")
 
-// Define variables for dataset; button, form, inputfield, and body selectors
 var tableData = data;
 var button = d3.select("#filter-btn");
-var form = d3.select("form");
+var form = d3.select("form")
 var inputField = d3.select("#datetime");
 var tbody = d3.select("tbody");
 
-// Loop through dataset and dynamically create a new row for each element and add that element's data to that row in the HTML table
 var insertData = tableData.forEach(function(ufoDataElement) {
     
     var row = tbody.append("tr");
@@ -16,8 +14,6 @@ var insertData = tableData.forEach(function(ufoDataElement) {
     });
 });
 
-// Create function to handle a click or 'enter' on the 'filter table button'
-// Filter the data based on the input value, clear the table, and rebuild the table based on the input value
 function handleEvent() {
 
     d3.event.preventDefault();
@@ -39,6 +35,5 @@ function handleEvent() {
     });
 };
 
-// When a click or submit is detected, run the above function to process it
 button.on("click", handleEvent);
 form.on("submit", handleEvent);
